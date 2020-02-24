448e5e9b6ab50eb0c1651e5d28f887d105e178d9
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2020-02-24 22:04:26 +0000

    yaw autoencoder

---
+++ VAE/train.py
@@ -0,0 +1,32 @@
+from model import AE
+from data_loader import PetDataLoader
+from torch.utils.tensorboard import SummaryWriter
+import torch
+import torchvision
+from tqdm import tqdm
+
+
+device=0
+
+model = AE(device)
+dataloader = PetDataLoader(device)
+writer = SummaryWriter()
+
+resume_epoch = 1  # if not args.resume else args.resume_epoch
+
+itr = 0
+for epoch in range(resume_epoch, 5 + 1):
+    print(epoch)
+    for batch in tqdm(dataloader):
+        loss = model.train_(batch)
+        print(loss.item())
+        writer.add_scalar("loss", loss.item(), itr)
+        recons = model.reconst(batch)
+        recons = torch.cat([batch["img"][:32].to(device), recons[:32]])
+        recons = torchvision.utils.make_grid(recons)
+        sample = model.sample_img(batch)
+        sample = torchvision.utils.make_grid(sample)
+        writer.add_image('recons', recons, itr)
+        writer.add_image('sample', sample, itr)
+        writer.add_scalar("loss", loss.item(), itr)
+        itr += 1
